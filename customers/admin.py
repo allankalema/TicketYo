2e7518503887be5ea81f3c79ad6cd4eec9773cb8
allankalema <allankalema4@gmail.com>
2024-08-26 10:18:29 +0300

    added searchable functionality to the  customers admin.py

--- customers/admin.py
+++ customers/admin.py
@@ -5,3 +5,4 @@ from .models import Customer
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('username','email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    search_fields = ('username','email', 'first_name', 'last_name', 'is_active')
