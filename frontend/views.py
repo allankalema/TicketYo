921386772ddae2b1aa6cea9e59935838af2cfb07
allankalema <allankalema4@gmail.com>
2024-09-01 17:56:14 +0300

    the universla login page made

--- frontend/views.py
+++ frontend/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'frontend/base.html')
+
+
+def universal_login_view(request):
+    return render(request, 'frontend/universal_login.html')
