4137ed8d445257e16dc38f868bb3bdd427f53be1
allankalema <allankalema4@gmail.com>
2024-09-03 08:14:00 +0300

    vendor signup done

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
      path('universal-login/', views.universal_login_view, name='universal_login'),
+      path('/forgot_password/', views.forgot_password, name='main-forgot_password'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def universal_login_view(request):
     return render(request, 'frontend/universal_login.html')
+
+def forgot_password(request):
+    return render(request, 'frontend/forgot-password.html')
