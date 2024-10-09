ee24e370357f409ba7aaecbb171b547382ec9b90
allankalema <allankalema4@gmail.com>
2024-10-09 17:30:53 +0300

    the is_posagent field added

--- pos/models.py
+++ pos/models.py
@@ -11,6 +11,7 @@ class POSAgent(models.Model):
     assigned_events = models.ManyToManyField(Event, related_name='assigned_agents')
     is_active = models.BooleanField(default=True)
     password = models.CharField(max_length=6)
+    is_posagent = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.username})"
