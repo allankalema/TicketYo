0450ad32b70cf603dd1207386d5055dd27cedfd0
allankalema <allankalema4@gmail.com>
2024-10-18 08:36:17 +0300

    correcting wat never changed int the conflicts

--- TicketYo/settings.py
+++ TicketYo/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'accounts.middleware.UserInstanceMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'TicketYo.urls'
