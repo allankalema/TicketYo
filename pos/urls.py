8d72b1dbb873cd3746ce2c421bd774391b1f4fc3
allankalema <allankalema4@gmail.com>
2024-10-21 18:34:29 +0300

    removed stuff

--- pos/urls.py
+++ pos/urls.py
@@ -3,11 +3,8 @@ from . import views
 
 urlpatterns = [
     path('manage_agents/', views.manage_pos_agents, name='manage_pos_agents'),
-    path('create_agent/', views.create_pos_agent, name='create_pos_agent'),
     path('agent_action/<int:agent_id>/', views.agent_action, name='agent_action'), 
     path('event_detail/<int:event_id>/', views.pos_event_detail, name='pos_event_detail'), 
-    path('verify-email/', views.verify_email, name='verify_email'),
-    path('signup/<str:email>/', views.signup_pos_agent, name='signup_pos_agent'),
     path('pos/dashboard/', views.pos_dashboard, name='pos_dashboard'), 
 
     path('inventory/', views.inventory_view, name='pos_inventory'),
