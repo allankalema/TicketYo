350ea1649f1e1f24505ce5ac21586fae5faf274c
SENYONJO HENRY <senyonjohenry1@gmail.com>
2024-10-22 06:20:49 +0300

    Designed the POS pages and the ticket pages

--- events/urls.py
+++ events/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.homepage, name='homepage'),
     path('create/', views.create_event, name='create_event'),
     path('my-events/', views.vendor_events, name='vendor_events'),
-    path('', views.all_events, name='all_events'),
+    path('all_events', views.all_events, name='all_events'),
     path('event/<int:event_id>/delete/', views.delete_event, name='delete_event'),
     path('events/<int:event_id>/update/', views.update_event, name='update_event'),
     # path('event/<int:event_id>/add/', views.add_to_cart, name='add_to_cart'),
@@ -14,5 +15,5 @@ urlpatterns = [
      path('vendor-event/<int:event_id>/', views.event_detail_view, name='vendor_event_detail'),
     path('past-events/', views.past_events_view, name='past_events'),
     path('approve-event/<int:event_id>/', views.dash_approve_event, name='dash_approve_event'),
-    path('home/',views.homepage, name='homepage'),
+    
 ]
